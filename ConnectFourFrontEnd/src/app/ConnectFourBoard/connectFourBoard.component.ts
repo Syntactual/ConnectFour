83ac7e187066a445bc6dd6ffc222e93d495bf2a8
Syntactual <elanjoshrand@gmail.com>
2018-01-16 21:57:23 -0500

    added component for the game board

---
+++ ConnectFourFrontEnd/src/app/ConnectFourBoard/connectFourBoard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'connectFourBoard',
+  templateUrl: './connectFourBoard.component.html',
+  styleUrls: ['./connectFourBoard.component.css']
+})
+export class ConnectFourBoard {
+  
+}
