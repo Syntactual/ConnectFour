005d048fbaac2d03d9b7e1bd27fb7a170d2f6d72
Oppenheim <Joshua.Oppenheim@STERICYCLE.com>
2018-01-19 15:19:57 -0500

    computer can stop user with horizontal and vertical; still needs diagonal test

--- ConnectFourFrontEnd/src/app/ConnectFourBoard/connectFourBoard.component.ts
+++ ConnectFourFrontEnd/src/app/ConnectFourBoard/connectFourBoard.component.ts
@@ -148,29 +148,68 @@ public playMaster(){
         if(!this.player1 && this.playingComputer){
        
           setTimeout(() => {
-            if(this.computerTestVertical(this.player1Positions).length !== 0){
+            
+            if(this.computerTestVertical(this.player1Positions).length !== 0 && this.computerTestVertical(this.player1Positions)[0]>=0){
+              //use last location
               let position = this.computerTestVertical(this.player1Positions);
-              
+             
               this.playerPicked(position[0], position[1], document.getElementById(position[0] + '-' + position[1]));
+              console.log("vertical");
             }else{
               //check player 1 for 3 in a row; if they have one see if it can play the winning slot
-              let locations = this.testForThree(this.player1Positions);
-              //console.log(locations);
-              if(locations.length !== 0){
-                if((typeof(this.board[locations[2]][locations[1]]) === 'undefined' && this.board[locations[2]][locations[1]+ 1])|| (typeof(this.board[locations[2]][locations[1]]) === 'undefined' && locations[1] === 5)) {
-                  console.log("first spot");
-                  this.playerPicked(locations[2], locations[1], document.getElementById(locations[2] + '-' + locations[1]));
-                }else if((typeof(this.board[locations[2]][locations[0]]) === 'undefined' && this.board[locations[2]][locations[0]+1]) || (typeof(this.board[locations[2]][locations[0]]) === 'undefined' && locations[0] === 5)){
-                  console.log("second spot");
-                  this.playerPicked(locations[2], locations[0], document.getElementById(locations[2] + '-' + locations[0]));
+              //use last location and also test for last missing slot
+
+              let positions = this.computerHorizontal(this.player1Positions);
+              console.log(positions);
+              if(positions.length !== 0){
+                let pickedMove = new Array();
+                for(let position of positions){
+                  //test for not being on row 5
+                   
+                  if(position[1]<=6 && position[1]>= 0 && typeof(this.board[position[0]][position[1]])==='undefined'){
+                    if(position[0] !== 5 && this.board[position[0]+ 1][position[1]] || position[0]===5){
+                      pickedMove.push([position[0],position[1]]);
+                      console.log("horizontal");
+                      break;
+                    }
+                    
+                  }
+                  
+                }
+                if(pickedMove.length !== 0){
+                  this.playerPicked(pickedMove[0][0], pickedMove[0][1], document.getElementById(pickedMove[0][0] + '-' + pickedMove[0][1]));
                 }else{
                   this.RandomPlay();
-                  console.log("random");
-                
+                  console.log("Random");
                 }
+                
+                
               }else{
                 this.RandomPlay();
+                console.log("Random");
               }
+              /*
+                if(locations[1] <= 6 ){
+                  if((typeof(this.board[locations[2]][locations[1]]) === 'undefined' && locations[2] === 5)|| (typeof(this.board[locations[2]][locations[1]]) === 'undefined' && this.board[locations[2] + 1][locations[1]])) {
+                    console.log("first spot");
+                    console.log(locations[2]+ " "+ locations[1]);
+                    this.playerPicked(locations[2], locations[1], document.getElementById(locations[2] + '-' + locations[1]));
+                  }else{
+                    this.RandomPlay();
+                  console.log("random");
+                  }
+                                   
+                }else if(locations[0] >= 0){
+                  if((typeof((this.board[locations[2]][locations[0]])) === 'undefined' && locations[2] === 5)|| (typeof(this.board[locations[2]][locations[0]]) === 'undefined' && this.board[locations[2]+1][locations[0]])) {
+                    console.log("second spot");
+                    this.playerPicked(locations[2], locations[0], document.getElementById(locations[2] + '-' + locations[0]));
+                  }else{
+                    this.RandomPlay();
+                  console.log("random");
+                  }
+                }*/
+               
+               
             }
             
            
@@ -220,7 +259,44 @@ public playMaster(){
 
   private computerTestVertical(playerPositions: Array<number>){
 
+    
+    let lastPosition = playerPositions[playerPositions.length-1];
     let newPosition = new Array();
+    let point = 1;
+    for(let x = 1; x <= 2; x++)
+    {
+      let pointHit = false;
+      for(let position of playerPositions){
+        if(position[0] === lastPosition[0] + x && position[1] === lastPosition[1]){
+          point++;
+          //console.log(point);
+          pointHit = true;
+          if(point === 3){
+            newPosition = [lastPosition[0]-1,lastPosition[1]];
+            //console.log(newPosition);
+            return newPosition;
+          }
+          break;
+        
+        }
+        
+      }
+     
+      /*
+      if(!pointHit){
+        break;
+      }
+    }
+    if(point == 3){
+      return true;
+    }else{
+      return false;
+    }
+    
+  }
+
+
+    
     let rowSequence = new Array();
     for(let position of playerPositions){
       rowSequence.push(position[0]);
@@ -283,7 +359,118 @@ public playMaster(){
       
     }
     return newPosition;
+    */
+    }
+    return newPosition;
   }
+
+  private computerHorizontal(playerPositions: Array<Array<number>>){
+    let nextPosition = [];
+    let rows = new Array(6);
+    for(let t = 0; t < 6; t++){
+      rows[t] = new Array();
+    }
+   
+      for(let position of playerPositions){
+        
+        switch(position[0]){
+          case 0 : rows[0].push(position[1]);
+                   break;
+          case 1 : rows[1].push(position[1]);
+                  break;
+          case 2 : rows[2].push(position[1]);
+                   break;
+          case 3 : rows[3].push(position[1]);
+                  break;
+          case 4 : rows[4].push(position[1]);
+                  break;
+          case 5 : rows[5].push(position[1]);
+                  break;
+                
+        }          
+      
+      }
+      let index = -1;
+      for(let row of rows){
+        index++;
+        row.sort();
+        let points = 1;
+        for(let i = 0; i < row.length; i++) {
+          //console.log(array[i] - array[i -1]);
+          if(row[i] - row[i-1] == 1) {
+          points++;
+        
+        if(points === 3)
+        {
+          nextPosition.push([index,row[0]-1]);
+          nextPosition.push([index,row[row.length -1]+1]);
+          break;
+        }   
+          }else{
+            points = 1;
+          }
+
+          
+          }
+      }
+      return nextPosition;
+
+
+
+/*
+    let lastPostion = playerPositions[playerPositions.length -1];
+    let points = 1;
+   
+    let notHitRight = false;
+    let notHitLeft = false;
+//get array of all column postions to look at sequencal order
+//only do horizontal right and then horizontal left
+for(let x = 1; x <= 2; x++)
+{ 
+
+  for(let position of playerPositions){
+    if(notHitLeft && notHitRight){
+      break;
+    }
+    if(!notHitRight){
+      if(position[0] === lastPostion[0] && position[1] === lastPostion[1] + x){
+        console.log("pointHorizontal");
+        points++;
+       // console.log(point);
+      
+        if(this.playingStandard && points === 3){
+          nextPosition.push([position[0],position[1]+1]);
+          notHitRight = true;
+           }
+         
+        }else{
+          notHitRight = true;
+        }
+    }
+   
+    if(!notHitLeft){
+      if(position[0] === lastPostion[0] && position[1] === lastPostion[1] - x){
+        console.log("pointHorizontal");
+        points++;
+        
+        if(this.playingStandard && points === 3){
+         
+            nextPosition.push([position[0],position[1]-1]);
+            notHitRight = true;
+           }
+            
+          }else{
+            notHitLeft = true;
+          }
+    }
+   
+       }
+
+      }
+    return nextPosition;
+    */
+  }
+  
 
   private testForThree(playerPositions: Array<number>){
     
@@ -565,7 +752,7 @@ let point = 1;
     return fourInARow;
       
 /*
-      for(let x = 1; x< row0.length; x++)
+      for(let x = 1; x< row.length; x++)
     }
     
     for(let x = 1; x <= 3; x++)
@@ -676,6 +863,7 @@ let point = 1;
       }
       if(points >= 3)
       {
+        
         return [array[0] -1, array[array.length -1]+ 1];
       }
       else{
